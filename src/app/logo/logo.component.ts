40c5c76d58964e7dcbcaea50c6b0606e4f568306
Tamás Nagy <ntamasalbert@gmail.com>
2025-01-03 22:02:51 +0100

    add reuseable logo component

---
+++ src/app/logo/logo.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  standalone: false,
+
+  templateUrl: './logo.component.html',
+  styleUrl: './logo.component.css',
+})
+export class LogoComponent implements OnInit {
+  @Input() classes: string[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // Get the classes for the logo
+  get getClasses(): string[] {
+    const hasHeightDefined = this.classes.some((c) => c.startsWith('h-'));
+    const hasDisplayDefined = this.classes.some((c) =>
+      ['block', 'inline-block', 'flex', 'inline', 'grid', 'hidden'].includes(c)
+    );
+
+    return [
+      ...(hasHeightDefined ? [] : ['h-1/2']),
+      ...(hasDisplayDefined ? [] : ['block']),
+      ...this.classes,
+    ];
+  }
+}
