89f806824f3fc2eba235d52e54ae6b265e704f9b
Tamás Nagy <ntamasalbert@gmail.com>
2025-01-07 16:41:07 +0100

    add page-heading component

---
+++ src/app/components/page-heading/page-heading.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-heading',
+  standalone: false,
+  
+  templateUrl: './page-heading.component.html',
+  styleUrl: './page-heading.component.css'
+})
+export class PageHeadingComponent {
+
+}
