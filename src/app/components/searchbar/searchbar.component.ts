4461a870ca5cc86428a32bc154cd9d878cddee8d
Tamás Nagy <ntamasalbert@gmail.com>
2025-01-10 11:28:55 +0100

    add reuseable searchbar component

---
+++ src/app/components/searchbar/searchbar.component.ts
@@ -0,0 +1,53 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
+import Day from '../../day';
+import Week from '../../week';
+
+@Component({
+  selector: 'app-searchbar',
+  standalone: false,
+
+  templateUrl: './searchbar.component.html',
+  styleUrl: './searchbar.component.css',
+})
+export class SearchbarComponent implements OnInit {
+  @Input() searchData: Day[] | Week[] = [];
+  @Input() actualData: Day[] | Week[] = [];
+  @Output() actualDataChange = new EventEmitter<Day[] | Week[]>();
+
+  filteredData: Observable<Day[] | Week[]> = new Observable<Day[] | Week[]>();
+  control = new FormControl('');
+
+  ngOnInit() {
+    this.filteredData = this.control.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value || ''))
+    );
+
+    this.filteredData.subscribe((data) => {
+      this.actualData = data;
+      this.actualDataChange.emit(this.actualData);
+      console.log(this.actualData);
+    });
+  }
+
+  // filter the days based on the input value
+  private _filter(value: string): Day[] | Week[] {
+    const filterValue = this._normalizeValue(value);
+    if (this.searchData.length > 0 && 'tasks' in this.searchData[0]) {
+      return (this.searchData as Day[]).filter((data) =>
+        this._normalizeValue(data.name).includes(filterValue)
+      );
+    } else {
+      return (this.searchData as Week[]).filter((data) =>
+        this._normalizeValue(data.name).includes(filterValue)
+      );
+    }
+  }
+
+  // make the query lowercase and remove spaces
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+}
