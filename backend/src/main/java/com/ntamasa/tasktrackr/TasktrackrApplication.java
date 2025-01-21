d65c0e383aad80c783966bad2abae3e551a41ab9
Tamás Nagy <ntamasalbert@gmail.com>
2025-01-21 16:20:37 +0100

    create frontend and backend folders

---
+++ backend/src/main/java/com/ntamasa/tasktrackr/TasktrackrApplication.java
@@ -0,0 +1,13 @@
+package com.ntamasa.tasktrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasktrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasktrackrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ntamasa/tasktrackr/TasktrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntamasa.tasktrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasktrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
